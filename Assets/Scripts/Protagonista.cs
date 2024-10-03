62c79acc2c3cbddda122b234ae8710e778103ae1
estef157 <estefaniaizarra8@gmail.com>
2024-10-03 21:25:40 +0200

    03/10

--- Assets/Scripts/Protagonista.cs
+++ Assets/Scripts/Protagonista.cs
@@ -5,20 +5,22 @@ using UnityEngine;
 public class Protagonista : MonoBehaviour
 {
   //  Vector3 direccion = new Vector3(0, 0, 0);
-      Vector3 salto = new Vector3(0, 5, 0);
+      
     [SerializeField] int fuerza = 0;
      Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        GetComponent<Rigidbody>();
+
+        float v = Input.GetAxisRaw("Vertical");
+        Vector3 salto = new Vector3(0, v, 0);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
