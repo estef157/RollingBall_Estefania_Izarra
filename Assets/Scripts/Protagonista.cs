8bba8d301456a1bf69b29dfae7cc7af36ae8178f
estef <estefaniaizarra8@gmail.com>
2024-11-06 14:30:16 +0100

    06/1111

--- Assets/Scripts/Protagonista.cs
+++ Assets/Scripts/Protagonista.cs
@@ -13,42 +13,62 @@ public class Protagonista : MonoBehaviour
     Rigidbody rb;
     private float v, h;
 
-    // Start is called before the first frame update
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
 
         h = Input.GetAxisRaw("Horizontal");
         v = Input.GetAxisRaw("Vertical");
-        Vector3 direccion  = new Vector3 (h,v,0).normalized;
+        Vector3 direccion  = new Vector3 (h,0,v).normalized;
         Saltar();
     }
 
     private void FixedUpdate()
     {
         rb.AddForce(new Vector3 (h, 0, v).normalized * fuerzaMov, ForceMode.Force);
+        rb.AddForce(new Vector3(h, 0, v).normalized * fuerzaMov, ForceMode.Impulse);
         rb.velocity += new Vector3(0, -10f, 0);
     }
     private void Saltar()
     {
-        //Vector3 salto = new Vector3(0, v, 0);
+        Vector3 salto = new Vector3(0, v, 0);
 
         if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if (DetectarSuelo() == true)
-            {
-                rb.AddForce(Vector3.up * fuerzaSalto, ForceMode.Impulse);
-            }
-        }
+        
+           if (DetectarSuelo() == true)
+           {
+               rb.AddForce(Vector3.up.normalized * fuerzaSalto, ForceMode.Impulse);
+           }
     }
+       
+            
+
+    
     bool DetectarSuelo()
     {
        bool resultado = Physics.Raycast(transform.position, new Vector3(0, -1, 0), distanciaDeteccionSuelo, queEsSuelo);
         return resultado;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Respawn"))
+        {
+            rb =gameObject.GetComponent<Rigidbody>();
+            if(rb != null )
+            {
+                //Detener por el momento la bola
+                rb.velocity = Vector3.zero;
+                // Posicion inicial
+                rb.MovePosition(new Vector3(33.8f, 7, -29.7f));
+                rb.useGravity = true; //reactivar
+            }
+        }
     }
 }
