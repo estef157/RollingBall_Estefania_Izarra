c112d3a9402fc0b29a0268dc0df25e86d83fc631
estef <estefaniaizarra8@gmail.com>
2024-11-10 21:22:52 +0100

    TENGO miedo

--- Assets/Scripts/Moneda.cs
+++ Assets/Scripts/Moneda.cs
@@ -2,14 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Unity.UI;
+using UnityEngine.SceneManagement;
 
 
 public class Moneda : MonoBehaviour
 {
-
+    [SerializeField] Vector3 vectorMov;
+    [SerializeField] float velocidadmov;
+    float timer = 0;
     [SerializeField] int velocidadRot;
     private int puntuacion;
-    [SerializeField] TMP_Text textoPuntuacion;
+    [SerializeField] TMP_Text textoPuntos;
     [SerializeField] Vector3 direccionRot;
  
     // Start is called before the first frame update
@@ -21,12 +25,19 @@ public class Moneda : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
         transform.Rotate( direccionRot * velocidadRot * Time.deltaTime);
-        
+        transform.Translate(vectorMov * velocidadmov * Time.deltaTime, Space.World);
+        if (timer >= 1f)
+        {
+            vectorMov = vectorMov * -1;
+            timer = 0;
+        }
+
     }
     private void OnTriggerEnter(Collider other)
     {
         puntuacion += 1;
-        textoPuntuacion.SetText("x" + puntuacion);
+        textoPuntos.SetText("x" + puntuacion);
     }
 }
